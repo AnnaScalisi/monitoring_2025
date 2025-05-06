35742f378dd41a350ace3949509aa56ed18b9caf
AnnaScalisi <162353027+AnnaScalisi@users.noreply.github.com>
2025-05-06 12:43:36 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -10,3 +10,12 @@ setwd("C://utente/Desktop/") # caution!!!! use slash and not backslash
 
 mato = rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+
+# Bands:
+# band 1 = near infrared
+# band 2 = red
+# band 3 = green
+
+plotRGB(mato, r=2, g=3, b=1) # blu è vegetazione perchè NIR è in b
+plotRGB(mato, r=2, g=1, b=3) # verde è vegetazione
+plotRGB(mato, r=1, g=2, b=3) # rosso è vegetazione
